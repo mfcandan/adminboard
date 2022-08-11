1029fb3d3e93696a70beb3a1dfed07356f70778b
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-08-12 00:33:53 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
 function App() {
-  return <div className="App">Fatih Candan</div>;
+  return (
+    <div className="App">
+      <Home />
+      <List />
+      <Login />
+      <New />
+      <Single />
+    </div>
+  );
 }
 
 export default App;
