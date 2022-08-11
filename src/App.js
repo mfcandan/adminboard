9587c5aeecf890b7fb18c2e96a73dbb5e505677c
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-08-12 00:43:44 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
@@ -7,11 +8,24 @@ import Single from "./pages/single/Single";
 function App() {
   return (
     <div className="App">
-      <Home />
-      <List />
-      <Login />
-      <New />
-      <Single />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="/products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
