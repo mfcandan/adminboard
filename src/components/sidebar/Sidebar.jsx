871501c5267387d9f009f807076eb575870e57ab
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-08-12 02:18:30 +0300

    sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,87 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import StoreOutlinedIcon from "@mui/icons-material/StoreOutlined";
+import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import PollIcon from "@mui/icons-material/Poll";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Admin Panel</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">Main</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">Lists</p>
+          <li>
+            <PersonOutlineOutlinedIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreOutlinedIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardOutlinedIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">Usefull</p>
+          <li>
+            <PollIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">Service</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">User</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import './home.scss'
 
 const Home = () => {
   return <div className="home">
-    home
+    <Sidebar/>
+    <div className="homeContainer">
+      container
+    </div>
   </div>;
 };
 
