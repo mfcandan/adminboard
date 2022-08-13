3ec82209c04b387f28c28a2ae0f299664ae592fc
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-08-14 02:10:57 +0300

    new product & user

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,10 +1,60 @@
-import React from "react";
-import './new.scss'
+import React, { useState } from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+import "./new.scss";
 
-const New = () => {
-  return <div className="new">
-    New
-  </div>;
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+
+  console.log(file);
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h2>{title}</h2>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadIcon className="icon" />
+                </label>
+                <input
+                  onChange={(e) => setFile(e.target.files[0])}
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                />
+              </div>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
