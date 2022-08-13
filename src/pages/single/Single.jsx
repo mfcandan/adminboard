0797febadff7d68f7d968ab1f6fb99e6f138f028
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-08-13 16:29:20 +0300

    userPage

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,10 +1,58 @@
 import React from "react";
-import './single.scss'
+import Chart from "../../components/chart/Chart";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import List from "../../components/table/Table";
+import "./single.scss";
 
 const Single = () => {
-  return <div className="single">
-    Single
-  </div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h3 className="title">Information</h3>
+            <div className="item">
+              <img
+                src="https://assets-global.website-files.com/5ec7dad2e6f6295a9e2a23dd/5edfa7c6f978e75372dc332e_profilephoto1.jpeg"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Janet Jackson</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janetjackson@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+90 505 123 1212</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Dalton st. Newyork USA</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending (Last 6 Months)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h3 className="title">Last Transactions</h3>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
