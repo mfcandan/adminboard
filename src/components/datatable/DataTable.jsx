fbaab19ee51e844cac801f163c7e8c15bb34494d
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-08-14 02:40:45 +0300

    router

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -1,19 +1,35 @@
 import "./dataTable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
+import { Link } from "react-router-dom";
 
 const DataTable = () => {
-  const actionColumn = [{ field: "action", headerName: "Action", width: 200, renderCell:()=>{
-    return (
-      <div className="cellAction">
-        <div className="viewButton">View</div>
-        <div className="deleteButton">Delete</div>
-      </div>
-    )
-  }}];
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <Link to="/users/test">
+              <div className="viewButton">View</div>
+            </Link>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
 
   return (
     <div className="dataTable">
+      <div className="dataTableTitle">
+        Add New User
+        <Link to="/users/new" className="addBtn">
+          Add New
+        </Link>
+      </div>
       <DataGrid
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,12 +11,15 @@ import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">Admin Panel</span>
+        <Link to="/">
+          <span className="logo">Admin Panel</span>
+        </Link>
       </div>
       <hr />
       <div className="center">
@@ -27,14 +30,18 @@ const Sidebar = () => {
             <span>Dashboard</span>
           </li>
           <p className="title">Lists</p>
-          <li>
-            <PersonOutlineOutlinedIcon className="icon" />
-            <span>Users</span>
-          </li>
-          <li>
-            <StoreOutlinedIcon className="icon" />
-            <span>Products</span>
-          </li>
+          <Link to="/users">
+            <li>
+              <PersonOutlineOutlinedIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products">
+            <li>
+              <StoreOutlinedIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
           <li>
             <CreditCardOutlinedIcon className="icon" />
             <span>Orders</span>
@@ -45,7 +52,7 @@ const Sidebar = () => {
           </li>
           <p className="title">Usefull</p>
           <li>
-            <PollIcon className="icon"/>
+            <PollIcon className="icon" />
             <span>Stats</span>
           </li>
           <li>
